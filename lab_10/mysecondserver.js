c8599ca2d01b48f5514465911336bd156a89bad1
JoshuaNewton <j.newton@rgu.ac.uk>
2025-01-23 11:25:26 +0000

    aa

--- lab_10/mysecondserver.js
+++ lab_10/mysecondserver.js
@@ -5,7 +5,7 @@ var currentdate = require('./mymodule');
 http.createServer(function (req, res) {
 
  res.writeHead(200, {'Content-Type': 'text/html'});
- res.write("The date and time is currently: "+ curendate.myDateTime());
+ res.write("The date and time is currently: "+ currentdate.myDateTime());
  res.end('Hello World!');
  
 }).listen(8080);
