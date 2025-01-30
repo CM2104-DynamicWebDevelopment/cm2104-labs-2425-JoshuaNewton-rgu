975f3cfe7c2f02d9d548eb6d7b04603a684f1c41
JoshuaNewton-rgu <j.newton@rgu.ac.uk>
2025-01-30 10:50:07 +0000

    routing

--- lab_11/server.js
+++ lab_11/server.js
@@ -4,4 +4,9 @@ var app = express();
 app.get('/', function (req, res) {
 res.send("Hello world by express");
 });
+app.get('/test' , function (req, res) {
+    res.send("this is route 2 ");
+});
+
+
 app.listen(8080);
