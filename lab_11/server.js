3878ec3f8bd8ca4e46d990df570498f8208c3303
JoshuaNewton-rgu <j.newton@rgu.ac.uk>
2025-01-30 10:54:30 +0000

    Jokes

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var Jokes = require('knock-knock-jokes');
+
+
 
 app.get('/', function (req, res) {
 res.send("Hello world by express");
@@ -7,6 +10,12 @@ res.send("Hello world by express");
 app.get('/test' , function (req, res) {
     res.send("this is route 2 ");
 });
+
+app.get('/joke' , function (req, res) {
+    var randomjoke = Jokes();
+    res.end(randomjoke);
+});
+
 
 
 app.listen(8080);
