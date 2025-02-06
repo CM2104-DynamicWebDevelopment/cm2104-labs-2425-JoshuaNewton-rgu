2399a91ab7c913cca34ecbe402ad17fb63327ce1
JoshuaNewton-rgu <j.newton@rgu.ac.uk>
2025-02-06 11:26:36 +0000

    aa

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,19 +1,12 @@
 var express = require("express");
-const SpotifyWebApi = require("spotify-web-api-node");
 var app = express();
-var spotifyWebApi = require("spotify-web-api-node");
 app.use(express.static("public"));
+const SpotifyWebApi = require("spotify-web-api-node");
 
 var spotifyApi = new SpotifyWebApi({
     clientID: '1ff8f943ebfc4f09b8cbcc0f1b39a0ec' ,
     clientSecret: '6e45c1307fe947228ff310e85c56f28f'
 
-});
-
-app.use(express.static("public"));
-
-app.get("/",function(req,res){
-    res.send("Hello World! By Express");
 });
 
 //Retrieving an access token
